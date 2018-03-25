7167964b12b8374466f3405090184c9304224cad
claudivan filho <claudivan.filho@splab.ufcg.edu.br>
2018-03-25 00:16:03 -0300

    remove protocol of external requests (to solve heroku issues)

--- src/components/GoogleMaps/index.js
+++ src/components/GoogleMaps/index.js
@@ -9,7 +9,7 @@ import {
 
 export default compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "//maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `300px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
